97698cf2cf2d373ee9204391bfc6eb3fcc6757ba
Priya <priyalghose@gmail.com>
2018-04-02 11:29:32 -0700

    Created tic tac toe board. Allowed users to click on a square to make a move.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,15 @@
+var move = 1;
 
-
+var makeMove = function(el) {
+	console.log('Congrats, youre in the makeMove function. FUCK YEAH');
+	if (move % 2 !== 0) {
+		// console.log('x')
+	var space = document.getElementById(el.id);
+	space.innerHTML += 'X';
+	} else {
+		// console.log('o')
+		var space = document.getElementById(el.id);
+		space.innerHTML += 'O'
+	}
+	move++;
+}

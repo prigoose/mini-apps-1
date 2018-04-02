542362000d77a2393f2518e1dd7e2d8a92f42267
Priya <priyalghose@gmail.com>
2018-04-02 12:07:20 -0700

    Added game board. Also, added a console log when either player wins by placing 3 on the same row

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,15 +1,59 @@
 var move = 1;
+var gameBoard = [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']]; // e = empty space
 
 var makeMove = function(el) {
-	console.log('Congrats, youre in the makeMove function. FUCK YEAH');
+	// Setup work
+	var id = el.id;
+	var row = id.split('')[0];
+	var col = id.split('')[1];
+
 	if (move % 2 !== 0) {
-		// console.log('x')
-	var space = document.getElementById(el.id);
-	space.innerHTML += 'X';
+		// Place X
+		var space = document.getElementById(id);
+		space.innerHTML += 'X';
+
+		// Keep track of game state
+		gameBoard[row][col] = 'X';
+
 	} else {
-		// console.log('o')
-		var space = document.getElementById(el.id);
+		var space = document.getElementById(id);
 		space.innerHTML += 'O'
+
+		// Keep track of game state
+		gameBoard[row][col] = 'O';
 	}
+
+	// CHECK RESULTS once each player has placed 3 pieces
+	if (move >= 5) {
+		// check rows for winner
+		for (var i=0; i < 3; i++) {
+			if (! gameBoard[i].includes('e')) {
+				if (! gameBoard[i].includes('X')) {
+					console.log('O wins on row ', i)
+				} else if (! gameBoard[i].includes('O')) {
+					console.log('X wins on row ', i)
+				}
+			}
+		}
+
+		// check columns for winner
+		// for (var i=0; i < 3; i++) {
+		// 	if 
+		// }
+
+		// check diagonals for winner
+
+
+	}
+
 	move++;
 }
+
+
+		// for (var i=0; i < 3; i++) {
+		// 	if (! gameBoard[i].includes('e')) {
+		// 		if (! gameBoard[i].includes('X')) {
+		// 			console.log('O wins!')
+		// 		}
+		// 	}
+		// }

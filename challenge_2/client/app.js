960fe266b1dbfb3033076fbc5992899f3dd3034a
Priya <priyalghose@gmail.com>
2018-04-03 16:38:54 -0700

    Express server is running, and talking to client

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,69 @@
+var exampleData = {
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker",
+    "sales": 1000000,
+    "children": [
+    {
+      "firstName": "Beth Jr.",
+      "lastName": "Johnson",
+      "county": "San Mateo",
+      "city": "Pacifica",
+      "role": "Manager",
+      "sales": 2900000,
+      "children": [
+        {
+          "firstName": "Smitty",
+          "lastName": "Won",
+          "county": "San Mateo",
+          "city": "Redwood City",
+          "role": "Sales Person",
+          "sales": 4800000,
+          "children": []
+        },
+        {
+          "firstName": "Allen",
+          "lastName": "Price",
+          "county": "San Mateo",
+          "city": "Burlingame",
+          "role": "Sales Person",
+          "sales": 2500000,
+          "children": []
+        }
+      ]
+    },
+    {
+      "firstName": "Beth",
+      "lastName": "Johnson",
+      "county": "San Francisco",
+      "city": "San Francisco",
+      "role": "Broker/Sales Person",
+      "sales": 7500000,
+      "children": []
+    }
+  ]
+};
 
+// Will hold any functions from client side. For example, on click, run post request
+var App = {};
 
+// initialization
+App.init = function() {
+	$( document ).ready(function() {
+		$("#btn").click(function(e) {
+			e.preventDefault();
+			console.log("You have clicked!");
+			$.ajax('/csv', {
+			  method: "POST",
+			  contentType: 'application/json',
+			  data: JSON.stringify(exampleData),
+			  success: null, // can call 'get' to give back data
+			  error: null
+			});
+		})	
+	})
+}
+
+App.init();
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
 
 
+// app.use((req, res, next) => {
+// 	console.log(`received a ${req.method} to ${req.url}`);
+// 	console.log(`req body: ${JSON.stringify(req.body)}, 
+// 				req.query: ${req.query},
+// 				req.params: ${req.params}`)
+// 	next();
+// })        
+
+app.use(bodyParser.json());   
+
+
+// App.use(express.static) is an easy way to serve your initial home page.
+// I think it's equivalent to app.get() {res.sendFile(index.html)}
+// this is also how we connect the server to the client side
+// we use server to render specified files at the specified url (in this case port 3000)
+app.use(express.static('./client'))
+
+// handle get requests when they send input into text field
+app.post('/csv', function(req, res) {
+	// console.log(req.body);
+	// console.log(typeof req.body);
+	fs.writeFile('results.csv', req.body.firstName, (err) => {
+  		if (err) throw err;
+  		console.log('The file has been saved!');
+	});
+})
+
+app.listen(3000, () => console.log('App is running on http://localhost:3000'))

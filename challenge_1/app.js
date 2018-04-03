f70e0240b67b241bfa7398bf9135096231b06e84
Priya <priyalghose@gmail.com>
2018-04-02 17:19:55 -0700

    Refactored code so that game state properties and methods live on a game state object, and view properties and method live on a view object. It was a bit tricky as I feel that there is a lot of overlap between View and Game State. For example, if Player 1 is assigned to 'X's, that seems to be important to both the game state and the view.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,46 +1,62 @@
-App = {
-	firstMove: 'X',
-	secondMove: 'O',
-	move: 1,
-	gameBoard: [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']], // e: empty space
-	xWins: 0,
-	oWins: 0,
-	p1Name: '',
-	p2Name: ''
-};
+// CONTROLLER FUNCTIONS
 
-App.makeMove = function(el) {
+var makeMove = function(el) { 
+	GameState.move++;
+	// make move is a controller fxn. When the user clicks, makeMove runs and 
+	// in turn updates the model and the view
+
 	// Setup work
 	var id = el.id;
 	var row = id.split('')[0];
 	var col = id.split('')[1];
 
-	if (App.move % 2 !== 0) {
-		// Place X
-		var space = document.getElementById(id);
-		space.innerHTML += App.firstMove;
+	// Update view
+	View.showMove(id);
 
-		// Keep track of game state
-		App.gameBoard[row][col] = App.firstMove;
+	// Update model
+	GameState.updateBoard(row, col);
+}
 
+resetGame = function(winner) {
+	View.clear();
+	View.updateWinsLosses(winner);
+	GameState.reset();
+}
+
+
+// GAME STATE CODE
+
+var GameState = { 
+	gameBoard: [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']], // e: empty space
+	move: 0,
+	winner: '',
+	xWins: 0, // should # of wins be on game state or presentation?
+	oWins: 0
+}
+
+GameState.updateBoard = function(row, col) {
+	// Update Board
+	if (GameState.move % 2 !== 0) {
+		GameState.gameBoard[row][col] = View.firstMove;
 	} else {
-		var space = document.getElementById(id);
-		space.innerHTML += App.secondMove
-
-		// Keep track of game state
-		App.gameBoard[row][col] = App.secondMove;
+		GameState.gameBoard[row][col] = View.secondMove;
 	}
-
+	
 	// CHECK RESULTS once each player has placed 3 pieces
-	if (App.move >= 5) {
-		// check rows for winner
+	if (GameState.move > 5) {
+		GameState.checkResults();
+	}
+}
+
+GameState.checkResults = function() {
+	// check rows for winner
 		for (var i=0; i < 3; i++) {
-			if (! App.gameBoard[i].includes('e')) {
-				if (! App.gameBoard[i].includes(App.firstMove)) {
-					App.winnerIs(App.secondMove);
+			if (! GameState.gameBoard[i].includes('e')) {
+				if (! GameState.gameBoard[i].includes(View.firstMove)) {
+					GameState.winnerIs(View.secondMove);
 					return;
-				} else if (! App.gameBoard[i].includes(App.secondMove)) {
-					App.winnerIs(App.firstMove);
+				} else if (! GameState.gameBoard[i].includes(View.secondMove)) {
+					GameState.winnerIs(View.firstMove);
 					return;
 				}
 			}
@@ -48,86 +64,116 @@ App.makeMove = function(el) {
 
 		// check columns for winner. 
 		for (var j=0; j < 3; j++) {
-			if (App.gameBoard[0][j] === App.gameBoard[1][j] && App.gameBoard[1][j] === App.gameBoard[2][j]) {
-				if (App.gameBoard[0][j] === App.firstMove) {
-					App.winnerIs(App.firstMove);
-					return;App.gameBoard = [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']];
-				} else if (App.gameBoard[0][j] === App.secondMove) {
-					App.winnerIs(App.secondMove);
+			if (GameState.gameBoard[0][j] === GameState.gameBoard[1][j] && GameState.gameBoard[1][j] === GameState.gameBoard[2][j]) {
+				if (GameState.gameBoard[0][j] === View.firstMove) {
+					GameState.winnerIs(View.firstMove);
+					return;GameState.gameBoard = [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']];
+				} else if (GameState.gameBoard[0][j] === View.secondMove) {
+					GameState.winnerIs(View.secondMove);
 					return;
 				}
 			}
 		}
 
 		// check major diagonals for winner
-		if (App.gameBoard[0][0] === App.gameBoard[1][1] && App.gameBoard[1][1] === App.gameBoard[2][2]) {
-			if (App.gameBoard[0][0] === App.firstMove) {
-				App.winnerIs(App.firstMove);
+		if (GameState.gameBoard[0][0] === GameState.gameBoard[1][1] && GameState.gameBoard[1][1] === GameState.gameBoard[2][2]) {
+			if (GameState.gameBoard[0][0] === View.firstMove) {					
+				GameState.winnerIs(View.firstMove);
 				return;
-			} else if (App.gameBoard[0][0] === App.secondMove) {
-				App.winnerIs(App.secondMove);
+			} else if (GameState.gameBoard[0][0] === View.secondMove) {
+				GameState.winnerIs(View.secondMove);
 				return;
 			}
 		}
 
 		//check minor diagonals for winner
-		if (App.gameBoard[2][0] === App.gameBoard[1][1] && App.gameBoard[1][1] === App.gameBoard[0][2]) {
-			if (App.gameBoard[1][1] === App.firstMove) {
-				App.winnerIs(App.firstMove);
+		if (GameState.gameBoard[2][0] === GameState.gameBoard[1][1] && GameState.gameBoard[1][1] === GameState.gameBoard[0][2]) {
+			if (GameState.gameBoard[1][1] === View.firstMove) {
+				GameState.winnerIs(View.firstMove);
 				return;
-			} else if (App.gameBoard[1][1] === App.secondMove) {
-				App.winnerIs(App.secondMove);
+			} else if (GameState.gameBoard[1][1] === View.secondMove) {
+				GameState.winnerIs(View.secondMove);
 				return;
 			}
 		}
+	
+		if (GameState.move === 9) {
+			GameState.winnerIs('Nobody');
+		}
 
-	}		
-	if (App.move === 9) {
-		App.winnerIs('Nobody');
-	}
-
-	App.move++;
 }
 
-App.winnerIs = function(winner) {
+GameState.winnerIs = function(winner) {
+
 	if (winner === 'X') {
-		App.xWins += 1;
+		GameState.xWins += 1;
 	} else if (winner === 'O') {
-		App.oWins += 1;
+		GameState.oWins += 1;
 	}
-	App.resetGame(winner);
+	
+	resetGame(winner);
+
 }
 
-App.resetGame = function(winner) {
+GameState.reset = function() {
+// Reset the game state
+	GameState.gameBoard = [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']];
+	GameState.move = 1;
+
+}
+
+
+// VIEW CODE
+
+var View = {
+	firstMove: 'X',
+	secondMove: 'O',
+	p1Name: '',
+	p2Name: ''
+};
+
+View.showMove = function(id) {
+	
+	if (GameState.move % 2 !== 0) {
+		// Place 'X'
+		var space = document.getElementById(id);
+		space.innerHTML += View.firstMove;
+	} else {
+		// Place 'O'
+		var space = document.getElementById(id);
+		space.innerHTML += View.secondMove
+	}
+
+}
+
+View.clear = function(winner) {
 
 	// Clear the game board
 	elementList = document.querySelectorAll("th");
 	for (var element=0; element < elementList.length; element++) {
 		elementList[element].innerHTML = '';
 	}
+}
 
-	// Reset the game state
-	App.gameBoard = [['e', 'e', 'e'],['e', 'e', 'e'],['e', 'e', 'e']];
-	App.move = 1;
-
+View.updateWinsLosses = function(winner) {
 	if (winner) { // if function was called due to a win
 		if (winner === 'Nobody') {
 			// first App.move stays the same
 		} else if (winner === 'X') {
-			App.firstMove = 'X';
-			App.secondMove = 'O';
+			View.firstMove = 'X';
+			View.secondMove = 'O';
 		} else if (winner === 'O') {
-			App.firstMove = 'O';
-			App.secondMove = 'X';
+			View.firstMove = 'O';
+			View.secondMove = 'X';
 		} 
-		document.getElementById("whoGoesFirst").innerHTML = `${App.p1Name} goes first`;
-		document.getElementById("winTally").innerHTML = `${App.p1Name}: ${App.xWins} points. ${App.p2Name}: ${App.oWins} points`;
+		document.getElementById("whoGoesFirst").innerHTML = `${View.p1Name} goes first`;
+		document.getElementById("winTally").innerHTML = `${View.p1Name}: ${GameState.xWins} points. ${View.p2Name}: ${GameState.oWins} points`;
 	}
 }
 
-App.setPlayerName = function(){
-	App.p1Name = document.getElementById("p1name").value;
-	App.p2Name = document.getElementById("p2name").value;
-	document.getElementById("playerPiece").innerHTML = `${App.p1Name}: X, ${App.p2Name}: O`;
-	document.getElementById("whoGoesFirst").innerHTML = `${App.p1Name} goes first`;
+View.setPlayerName = function(){
+	View.p1Name = document.getElementById("p1name").value;
+	View.p2Name = document.getElementById("p2name").value;
+	document.getElementById("playerPiece").innerHTML = `${View.p1Name}: X, ${View.p2Name}: O`;
+	document.getElementById("whoGoesFirst").innerHTML = `${View.p1Name} goes first`;
 }

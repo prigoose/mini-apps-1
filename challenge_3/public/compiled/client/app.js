535a07decca8eaea9205e77deb352725151749ad
Priya <priyalghose@gmail.com>
2018-04-04 19:57:29 -0700

    Last commit before Danny's lecture. Except everything broke, so it may be better to refer to my second-to-last commit

--- challenge_3/public/compiled/client/app.js
+++ challenge_3/public/compiled/client/app.js
@@ -4,7 +4,8 @@ class App extends React.Component {
 		super();
 		this.state = {
 			board: [['white', 'white', 'white', 'white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white', 'white', 'white', 'white'], ['white', 'white', 'white', 'white', 'white', 'white', 'white']],
-			player: 'blue'
+			player: 'blue',
+			winner: null
 		};
 		this.drop = this.drop.bind(this);
 	}
@@ -28,10 +29,40 @@ class App extends React.Component {
 		} else {
 			this.state.player = 'blue';
 		}
-		// look through all rows in that column, starting at
-		// for (var i=5; i >=0; i--) {
-		// 	//
-		// }			
+
+		this.checkWinner();
+	}
+
+	checkWinner() {
+		var counter = 0;
+		// see if anyone won on rows. 
+		for (var i = 0; i < 5; i) {
+			for (var j = 0; j < 6; j++) {
+				if (this.state.board[i][j] === 'blue') {
+					counter += 1;
+				} else {
+					counter = 0;
+				}
+				if (counter === 4) {
+					winner = 'blue';
+					return;
+				}
+			}
+		}
+
+		for (var i = 0; i < 5; i) {
+			for (var j = 0; j < 6; j++) {
+				if (this.state.board[i][j] === 'white') {
+					counter += 1;
+				} else {
+					counter = 0;
+				}
+				if (counter === 4) {
+					winner = 'white';
+					return;
+				}
+			}
+		}
 	}
 
 	render() {
